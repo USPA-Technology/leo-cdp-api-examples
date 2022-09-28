6e098f40c9c09fed9c4ee4ce6bcaa30efb467432
Trieu Nguyen <tantrieuf31.database@gmail.com>
2022-09-28 14:06:58 +0700

    update meta data for profile API

--- nodejs/src/save-profile.js
+++ nodejs/src/save-profile.js
@@ -6,20 +6,31 @@ async function makeRequest(processData) {
     url: "https://vnuk.leocdp.net/api/profile/save",
     data: {
       journeyMapId: "id_default_journey",
+      dataLabels: "DATA_FROM_LEO_API; CRM",
+      crmRefId: "123456",
       primaryAvatar	: "https://media.vneconomy.vn/w800/images/upload/2022/06/16/im-564446.jpg",
       primaryEmail: "media@gatesfoundation.org",
+      secondaryEmails: "",
       primaryPhone: "(206) 709-3400",
-      crmRefId: "123456",
+      secondaryPhones: "",
       firstName: "Bill",
       middleName: "",
       lastName: "Gates",
-      gender: "male",
+      gender: "male", // or female
       dateOfBirth: "1955-10-28", // yyyy-MM-dd
-      dataLabels: "test; demo; fake data; CRM",
-      totalCLV: 114.2,
-      totalCAC: 10.1,
-      totalTransactionValue: 20.25,
-      jobTitles: "software developer; investor; author; philanthropist",
+      livingLocation: "", // the address of customer 
+      livingCity: "", // the city where customer is living
+      jobTitles: "", // the Job Title, e.g: CEO; Manager; Head of Sales
+      workingHistory: "",
+      mediaChannels: "", // reachable media channels, E.g: facebook; linkedin; chat
+      personalInterests: "", 
+      contentKeywords: "",
+      productKeywords: "",
+      totalCLV: 0,
+      totalCAC: 0,
+      totalTransactionValue: 0 ,
+      saleAgencies: "", // the list of sales sources, e.g: Agency A; Agency B
+      saleAgents: "" // the list of  sales persons, e.g: Mr.Thomas; Ms.Van
     },
     headers: {
       tokenkey: "default_access_key",
