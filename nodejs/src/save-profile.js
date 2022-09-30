b295003cb4b58730ee5a3c21660b30b9ec63b052
Trieu Nguyen <tantrieuf31.database@gmail.com>
2022-09-30 11:05:47 +0700

    update example data

--- nodejs/src/save-profile.js
+++ nodejs/src/save-profile.js
@@ -6,9 +6,9 @@ async function makeRequest(processData) {
     url: "https://vnuk.leocdp.net/api/profile/save",
     data: {
       journeyMapId: "id_default_journey",
-      dataLabels: "DATA_FROM_LEO_API; CRM",
+      dataLabels: "DATA_FROM_LEO_API; CRM; KOL person",
       crmRefId: "123456",
-      primaryAvatar	: "https://media.vneconomy.vn/w800/images/upload/2022/06/16/im-564446.jpg",
+      primaryAvatar	: "https://cdn.britannica.com/47/188747-050-1D34E743/Bill-Gates-2011.jpg",
       primaryEmail: "media@gatesfoundation.org",
       secondaryEmails: "",
       primaryPhone: "(206) 709-3400",
@@ -20,17 +20,17 @@ async function makeRequest(processData) {
       dateOfBirth: "1955-10-28", // yyyy-MM-dd
       livingLocation: "", // the address of customer 
       livingCity: "", // the city where customer is living
-      jobTitles: "", // the Job Title, e.g: CEO; Manager; Head of Sales
-      workingHistory: "",
-      mediaChannels: "", // reachable media channels, E.g: facebook; linkedin; chat
-      personalInterests: "", 
-      contentKeywords: "",
-      productKeywords: "",
-      totalCLV: 0,
-      totalCAC: 0,
-      totalTransactionValue: 0 ,
-      saleAgencies: "", // the list of sales sources, e.g: Agency A; Agency B
-      saleAgents: "" // the list of  sales persons, e.g: Mr.Thomas; Ms.Van
+      jobTitles: "CEO; Chairman", // the Job Title, e.g: CEO; Manager; Head of Sales
+      workingHistory: "IBM; Microsoft",
+      mediaChannels: "website; facebook; linkedin;", // reachable media channels, E.g: facebook; linkedin; chat
+      personalInterests: "coding; business; investment; philanthropist", 
+      contentKeywords: "history; microsoft; product manament",
+      productKeywords: "Excel; Word; Microsoft Cloud",
+      totalCLV: 1000, // this is example data
+      totalCAC: 99, // this is example data
+      totalTransactionValue: 200 , // this is example data
+      saleAgencies: "Agency A; Agency B; Agency C", // the list of sales sources
+      saleAgents: "Mr.Thomas; Ms.Van" // the list of sales persons
     },
     headers: {
       tokenkey: "default_access_key",
