22fd778118a5a5d9e5010841400e2bb75a1aec6c
trieu <tantrieuf31.database@gmail.com>
2024-09-17 13:30:15 +0700

    update example test

--- python/cdp-event-api/save-tracking-event-online.py
+++ python/cdp-event-api/save-tracking-event-online.py
@@ -26,15 +26,15 @@ eventdata = {"itemId": "2019010113333", "idType": "SKU",
              "quantity": 2, "ghi chú": "khuyến mãi 20% <b> rất hot </b>"}
 tracking_event = {
     # the target update profile's email
-    'eventtime': '2024-09-16T19:57:25.110Z',
+    'eventtime': '2024-09-17T19:57:25.110Z',
     'targetUpdateEmail': "bill.john123@example.com",
-    'tpname': "Everon 2024",  # TOUCHPOINT_NAME
-    'tpurl': "https://www.everonvn.vn/chi-tiet/everon-epc24041.html",  # TOUCHPOINT_URL
+    'tpname': "Bộ Everon Esm-21016",  # TOUCHPOINT_NAME
+    'tpurl': "https://everonvn.com.vn/everon/3662/bo-everon-epm-24060",  # TOUCHPOINT_URL
     'tprefurl': "https://google.com",  # TOUCHPOINT_REFERRER_URL
     'eventdata': eventdata,  # custom event data
-    'imageUrls': "https://www.everonvn.vn/chi-tiet/images/upload/hinhanh/EPC-24041.jpg",
+  #  'imageUrls': "https://www.everonvn.vn/chi-tiet/images/upload/hinhanh/EPC-24041.jpg",
     'metric': test_metric,
-    'tsval': 5120100,
+    'tsval': 2015000,
     'tscur': 'VND',
     'tsstatus': 'OK',
     
@@ -50,20 +50,32 @@ tracking_event = {
 
 
 if test_metric == 'purchase':
-    shoppingCartItems = []
-    shoppingCartItems.append({
-        "name": "Bộ chăn bốn mùa Sắc Hạ ESC23002",
+    shoppingCartItems = [{
+        "name": "Bộ Everon Esm-21016",
         "itemid": "2750",
         "idtype": "item_ID",
-        "originalprice": 5299000,
-        "saleprice": 5299000,
+        "originalprice":  2239000,
+        "saleprice": 2015000,
         "quantity": 1,
         "currency": "VND",
         "supplierid": "",
         "couponcode": "",
-        "fullurl": "https://everon.com/bo-chan-bon-mua/bo-chan-bon-mua-sac-ha-esc23002-p2750.html",
-        "imageurl": "https://everon.com/images/products/2022/10/05/compress_square/4-mua_1664934649.jpg"
-    })
+        "fullurl": "https://everonvn.com.vn/everon/3662/bo-everon-epm-24060",
+        "imageurl": "https://everonvn.com.vn/media/CK/images/2024/epm-24060.jpg"
+    },
+    {
+        "name": "Bộ Everon Esm-21017",
+        "itemid": "2751",
+        "idtype": "item_ID",
+        "originalprice":  2239000,
+        "saleprice": 2015000,
+        "quantity": 1,
+        "currency": "VND",
+        "supplierid": "",
+        "couponcode": "",
+        "fullurl": "https://everonvn.com.vn/everon/3662/bo-everon-epm-24060",
+        "imageurl": "https://everonvn.com.vn/media/CK/images/2024/epm-24060.jpg"
+    }]
     transaction_id = "DEMO_TRANSACTION_" + now.strftime("%m/%d/%Y, %H:%M:%S")
     tracking_event['scitems'] = shoppingCartItems
     tracking_event['tsid'] = transaction_id
