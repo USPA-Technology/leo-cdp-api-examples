755e28918a20163daffb4a84ed74538d98489396
trieu <tantrieuf31.database@gmail.com>
2024-10-17 18:53:09 +0700

    update example code

--- python/cdp-event-api/save-tracking-event-service.py
+++ python/cdp-event-api/save-tracking-event-service.py
@@ -36,26 +36,56 @@ tracking_event = {
     'locationAddress' : "94 Đ. Võ Thị Sáu, Phường Tân Định, Quận 1, Hồ Chí Minh 700000"
 }
 
-
+# SERVICE 
 serviceitems = [{
+    "servicecode": "hcm-002",
+
     "name": "Nâng dưỡng làn da lụa là không tỳ vết",
+    "description": "test item",
     "imageurl":"https://fuchsiaspa.com/wp-content/uploads/Facials_Spa-01.jpg",
     "source": "Spa HCM",
-    "servicecode": "hcm-002",
+    
     "categoryid": "spa",
     "categoryname":  "Skin Care",
-    "originalprice": 600000,
-    "saleprice": 450000,
+    
+    #"originalprice": 600000,
+    #"saleprice": 450000,
+    #"currency": "VND",
+    
     "quantity": 3,
     "giftquantity" : 1,
-    "currency": "VND",
+   
     "expiredat" : '2025-01-01T00:00:01+07:00'
 }]
-
 tracking_event['serviceitems'] = serviceitems
+
+# PRODUCTS
+shoppingItems = [
+{
+    "productcode" : "T00083L01",
+    
+    "name": "[T00083L01] Collagen Gold (New)_1pcs",
+    "description": "test item",
+    "imageurl":"https://fuchsiaspa.com/wp-content/uploads/Facials_Spa-01.jpg",
+    "source": "Spa HCM",
+
+    "categoryid": "spa",
+    "categoryName": "product",
+    
+    "originalprice":  100000,
+    "saleprice": 50000,
+    "currency": "VND",
+    
+    "quantity": 10,
+   
+    "inpackage": 1,
+    "useservice": 0,
+    "serviceavailable":0
+}]
+tracking_event['scitems'] = shoppingItems
+
 transaction_id = "DEMO_TRANSACTION_" + now.strftime("%m/%d/%Y, %H:%M:%S")
 tracking_event['tsid'] = transaction_id
-
 
 json_payload = json.dumps(tracking_event)
 uri = '/api/event/save'
